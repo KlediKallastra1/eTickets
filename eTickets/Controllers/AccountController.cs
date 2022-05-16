7c5382f94e6e34ea61a3ff1d809395e20392a493
kledi <kledikallastra13@gmail.com>
2022-05-16 15:29:59 +0200

    Added User Profile Function

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -21,10 +21,42 @@ namespace eTickets.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult >Users()
+        public async Task<IActionResult> Users()
         {
             var users = await _context.Users.ToListAsync();
             return View(users);
+        }
+
+        public async Task<IActionResult> UserProfile()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserProfile([Bind("Id,FirstName,LastName,Email")] ApplicationUser userUpdate)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                TempData["Error"] = "An Error Occurred. Please contact an administrator.";
+                return View(userUpdate);
+            }
+
+            var checkEmail = await _userManager.FindByEmailAsync(userUpdate.Email);
+            if(checkEmail != null)
+            {
+                TempData["Error"] = "This email address already exists";
+                return View(userUpdate);
+            }
+
+            user.Email = userUpdate.Email;
+            user.FirstName = userUpdate.FirstName;
+            user.LastName = userUpdate.LastName;
+
+            await _userManager.UpdateAsync(user);
+
+            return RedirectToAction("Index", "Movies");
         }
 
         public IActionResult Login() => View(new LoginViewModel());
@@ -34,28 +66,30 @@ namespace eTickets.Controllers
         {
             if (!ModelState.IsValid) return View(model);
 
-            var user = await _userManager.FindByEmailAsync(model.Email);
-            if(user != null)
+            var userEmail = await _userManager.FindByEmailAsync(model.Email);
+            if (userEmail == null)
             {
-                var passwordCheck = await _userManager.CheckPasswordAsync(user, model.Password);
-                if (passwordCheck)
-                {
-                    var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
-                    if (result.Succeeded)
-                    {
-                        return RedirectToAction("Index", "Movies");
-                    }
-                }
+                TempData["Error"] = "Wrong Email Address. Please, try again";
+                return View(model);
+            }
+            var passwordCheck = await _userManager.CheckPasswordAsync(userEmail, model.Password);
+            if (!passwordCheck)
+            {
                 TempData["Error"] = "Wrong Password. Please, try again";
                 return View(model);
             }
-            
-            TempData["Error"] = "Wrong Email Address. Please, try again";
-            return View(model);
+            var result = await _signInManager.PasswordSignInAsync(userEmail, model.Password, false, false);
+            if (!result.Succeeded)
+            {
+                TempData["Error"] = "An Error Occurred. Please contact an administrator.";
+                return View(model);
+            }
+            return RedirectToAction("Index", "Movies");
+
         }
 
         [HttpPost]
-        public async Task<ActionResult> Logout() 
+        public async Task<ActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Movies");
@@ -69,7 +103,7 @@ namespace eTickets.Controllers
             if (!ModelState.IsValid) return View(model);
 
             var user = await _userManager.FindByEmailAsync(model.Email);
-            if(user != null)
+            if (user != null)
             {
                 TempData["Error"] = "This email address is already in use";
                 return View(model);
